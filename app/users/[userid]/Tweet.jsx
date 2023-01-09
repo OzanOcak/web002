26a7bc0559c2d0b9e44cae7bdf79f8b5aa263414
OzanOcak <ozanocak@gmail.com>
2023-01-09 09:30:26 -0500

    tweet end-point, prisma gunctions and hanlers in api

--- app/users/[userid]/Tweet.jsx
+++ app/users/[userid]/Tweet.jsx
@@ -1,17 +1,46 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as HartIconFilled } from "@heroicons/react/24/solid";
 
 const Tweet = ({ tweet }) => {
-  const [likes, setLikes] = useState(1);
+  const [numberOflikes, setNumberOfLikes] = useState(tweet?.numberOflikes);
+  const [liked, setLiked] = useState(null);
+
+  useEffect(() => {
+    if (liked === null) return;
+
+    const endpoint = liked
+      ? `/api/tweets/${tweet.id}/like`
+      : `/api/tweets/${tweet.id}/unlike`;
+
+    fetch(endpoint, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(({ tweet }) => {
+        setNumberOfLikes(tweet?.numberOflikes);
+      });
+  }, [liked]);
+
+  const handleClick = () => setLiked((liked) => !liked);
 
   return (
     <li className="my-4 rounded bg-stone-200 p-4 text-stone-600">
       <div className="flex justify-between">
         <p>{tweet.body}</p>
-        <button>
-          <span>{likes}</span>
+        <button onClick={handleClick}>
+          {liked ? (
+            <HartIconFilled className="w-7 h-7 text-red-500" />
+          ) : (
+            <HeartIcon className="w-7 h-7 text-red-500" />
+          )}
         </button>
+        <span className="text-sm">{numberOflikes}</span>
       </div>
     </li>
   );
